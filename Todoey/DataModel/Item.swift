24eec8f3c4479ed7225182c0de62eba1214088ea
Benjamin Weiss <benjamin1229@gmail.com>
2018-09-23 16:48:08 -0400

    Concise features added

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Benjamin Weiss on 9/21/18.
+//  Copyright © 2018 Benjamin Weiss. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false 
+}
+
---
+++ Todoey/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Todoey
+//
+//  Created by Benjamin Weiss on 9/21/18.
+//  Copyright © 2018 Benjamin Weiss. All rights reserved.
+//
+
+import UIKit
+
+class Item: {
+    
+}
